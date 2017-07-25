bc2b474ee9049f68962af84b493a8aec9c680871
juniorro <juniorro98@outlook.com>
2017-07-24 20:03:41 -0700

    finish client application

--- src/main/java/com/juniorro/servicecompany/controller/ServiceController.java
+++ src/main/java/com/juniorro/servicecompany/controller/ServiceController.java
@@ -36,26 +36,18 @@ public class ServiceController {
 	@RequestMapping(value = "/saveService", method = RequestMethod.POST) 
 	public ModelAndView saveService(@Valid Services service, Principal principal, Authentication authentication,  BindingResult result, Model model, RedirectAttributes redirect) {
 		if(service.getServiceDate() == null){
-			List <Services> allServices = servicesService.allServices();
-			model.addAttribute("allServices", allServices);
 			redirect.addFlashAttribute("noDate", true);
 			return new ModelAndView("redirect:/", "service", new Services());			
 		}		
 		if(service.getServiceDate().before(new Date())){
-			List <Services> allServices = servicesService.allServices();
-			model.addAttribute("allServices", allServices);
 			redirect.addFlashAttribute("greaterDate", true);
 			return new ModelAndView("redirect:/", "service", new Services());			
 		}
 		if(service.getName() == null){
-			List <Services> allServices = servicesService.allServices();
-			model.addAttribute("allServices", allServices);
 			redirect.addFlashAttribute("noName", true);
 			return new ModelAndView("redirect:/", "service", new Services());			
 		}
 		if (result.hasErrors()) {
-			List <Services> allServices = servicesService.allServices();
-			model.addAttribute("allServices", allServices);
 			redirect.addFlashAttribute("error", true);
 			return new ModelAndView("redirect:/", "service", new Services());
 		}
@@ -66,26 +58,46 @@ public class ServiceController {
 		service.setDescription("Sweeping, Mopping Floor.");
 		servicesService.saveService(service);
 		eventPublisher.publishEvent(new OnNewServiceRequest(systemUser, service));
-		List <Services> allServices = systemUser.getServices();
-		model.addAttribute("allServices", allServices);
 		redirect.addFlashAttribute("newService", true);
 		return new ModelAndView("redirect:/", "service", new Services());
+	}
+	
+	@RequestMapping(value = "/updateService", method = RequestMethod.POST) 
+	public ModelAndView updateService(@Valid Services service, Principal principal, Authentication authentication,  BindingResult result, Model model, RedirectAttributes redirect) {
+		if(service.getServiceDate() == null){
+			redirect.addFlashAttribute("noDate", true);
+			return new ModelAndView("redirect:/serviceInfo?id="+service.getId(), "service", new Services());			
+		}		
+		if(service.getServiceDate().before(new Date())){
+			redirect.addFlashAttribute("greaterDate", true);
+			return new ModelAndView("redirect:/serviceInfo?id="+service.getId(), "service", new Services());			
+		}
+		if(service.getName() == null){
+			redirect.addFlashAttribute("noName", true);
+			return new ModelAndView("redirect:/serviceInfo?id="+service.getId(), "service", new Services());			
+		}
+		if (result.hasErrors()) {
+			redirect.addFlashAttribute("error", true);
+			return new ModelAndView("redirect:/serviceInfo?id="+service.getId(), "service", new Services());
+		}
+		SystemUser systemUser = systemUserService.findByUsername(principal.getName());
+		service.setSystemUser(systemUser);
+		service.setRequestDate(new Date());
+		service.setStatus("pending");			
+		service.setDescription("Sweeping, Mopping Floor.");
+		servicesService.saveService(service);
+		redirect.addFlashAttribute("updateService", true);
+		return new ModelAndView("redirect:/serviceInfo?id="+service.getId(), "service", new Services());
 	}
 	
 	@RequestMapping(value = "/deleteService", method = RequestMethod.GET)
     public ModelAndView delete(@RequestParam("id") Long id, Principal principal, Model model, RedirectAttributes redirect) {
 		Services service = servicesService.getOne(id);
 		if(service.getStatus().contains("completed")){
-			SystemUser systemUser = systemUserService.findByUsername(principal.getName());
-			List <Services> allServices = systemUser.getServices();
-			model.addAttribute("allServices", allServices);
 			redirect.addFlashAttribute("cannotDelete", true);
 			return new ModelAndView("redirect:/", "service", new Services());
 		}
 		servicesService.delete(service);
-		SystemUser systemUser = systemUserService.findByUsername(principal.getName());
-		List <Services> allServices = systemUser.getServices();
-		model.addAttribute("allServices", allServices);
 		redirect.addFlashAttribute("serviceDeleted", true);
         return new ModelAndView("redirect:/");
     }
